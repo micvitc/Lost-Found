8d0e7aaeb7c78e4b977b6c3629b769b818432e13
Ramsaiharibabu <94447207+Ramsaiharibabu@users.noreply.github.com>
2023-06-13 00:23:51 +0530

    Update app.js

--- Backend/server/app.js
+++ Backend/server/app.js
@@ -1,26 +1,21 @@
 const mongoose = require('mongoose');
 const express = require('express');
-
+const dotenv = require("dotenv");
+dotenv.config({path: './config.env'});
 const app = express();
-const DB = "mongodb+srv://haribabu91000:Hari91000@cluster0.arzngib.mongodb.net/lostandfoundmain?retryWrites=true&w=majority"
-
-mongoose.connect(DB,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-
-}).then(()=>{
-    console.log(`connection successful`);
-}).catch((err)=>
-console.log(err)
-);
+const PORT = process.env.PORT
+require ("./db/conn")
+app.use(express.json())
+app.use((require('./router/auth')))
+const User = require("./schema/users")
+const Lostitemdata = require("./schema/lostItemData")
 app.get('/', (req, res) => {
-    res.send('hello hari i am from server ')
+    res.send('hello guys i am from server ')
 })
 app.get('/login', (req, res) => {
     res.send('this is from login page ')
 })
 
-app.listen(3000, () => {
-console.log("server code is runnimg")
+app.listen(PORT, () => {
+console.log("server code is running")
 })
-
